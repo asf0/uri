7dc678180a512b899d5e1c8b842335b845c592d0
ataide <scorpionspfc@gmail.com>
2018-09-05 20:05:52 -0300

    new

--- uri1045/Main.java
+++ uri1045/Main.java
@@ -4,24 +4,42 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        float a, b, c,;
-        a = sc.nextFloat();
-        b = sc.nextFloat();
-        c = sc.nextFloat();
+        double a, b, c, temp;
+        a = sc.nextDouble();
+        b = sc.nextDouble();
+        c = sc.nextDouble();
+        if (a < b) {
+            temp = a;
+            a = b;
+            b = temp;
+        } if (b < c) {
+            temp = b;
+            b = c;
+            c = temp;
+        }   if (a < b) {
+            temp = a;
+            a = b;
+            b = temp;
+        }
 
         if (a >= (b + c)) {
             System.out.println("NAO FORMA TRIANGULO");
-        }else {
-            if ((a*a) == ((b*b) + (c*c))) {
-                System.out.println("TRIANGULO RETANGULO");
-            } else if ((a*a) >= ((b*b) + (c*c))) {
-                System.out.println("TRIANGULO OBTUSANGULO");
-            } else if ((a*a) <= ((b*b) +(c*c))) {
-                System.out.println("TRIANGULO ACUTANGULO");
-            } else if (a == b == c) {
-                System.out.println("TRIANGULO EQUILATERO");
-            }
         }
-
+        else if (a*a > ((b*b)+(c*c))) {
+            System.out.println("TRIANGULO OBTUSANGULO");
+        }
+        if ((a*a) == ((b*b) + (c*c))) {
+            System.out.println("TRIANGULO RETANGULO");
+        }
+        if ((a*a) < (((b*b) +(c*c)))) {
+            System.out.println("TRIANGULO ACUTANGULO");
+        }
+        if (a == b && b == c && c == a) {
+            System.out.println("TRIANGULO EQUILATERO");
+        }
+        if ((a == b) && (a != c) || ((a == c) && (a != b)) || ((b == c) && (b !=a))) {
+            System.out.println("TRIANGULO ISOSCELES");
+        }
+        }
     }
-}
+
